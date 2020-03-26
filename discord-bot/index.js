e8a5a56b395f5ed781fad9198457ba50ef75ce73
Koray Koska <koray@koska.at>
2020-03-26 03:19:41 +0100

    Initial Commit

---
+++ discord-bot/index.js
@@ -0,0 +1,112 @@
+const ffmpeg = require("fluent-ffmpeg");
+const Discord = require("discord.js");
+const fs = require("fs");
+
+const client = new Discord.Client();
+
+const AudioMixer = require("audio-mixer");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", async message => {
+  var elements = message.content.trim().split(" ");
+  if (elements.length < 1) {
+    return;
+  }
+
+  switch (elements[0]) {
+    case "/ping":
+      message.reply("pong");
+      break;
+    case "/join":
+      // Only try to join the sender's voice channel if they are in one themselves
+      // console.log(message);
+      if (message.member.voice.channel) {
+        // Creates a new audio mixer with the specified options
+        let mixer = new AudioMixer.Mixer({
+          channels: 2,
+          bitDepth: 16,
+          sampleRate: 48000,
+          clearInterval: 250
+        });
+        mixer.highWatermark = 512;
+
+        const connection = await message.member.voice.channel.join();
+        // Connection is an instance of VoiceConnection
+        message.reply("I have successfully connected to the channel!");
+
+        const ff = ffmpeg("chefbenzos.monitor")
+          .inputFormat("pulse")
+          .outputFormat("s16le")
+          .outputOptions("-ac 2")
+          .outputOptions("-ar 48.0k")
+          .outputOptions("-bufsize 512");
+        ff.on("error", (err, stdout, stderr) => {
+          console.log(err);
+          console.log(stdout);
+          console.log(stderr);
+        });
+
+        const shotstream = ff.pipe();
+        // shotstream.highWatermark = 512;
+        connection.play(shotstream, {
+          type: "converted",
+          highWaterMark: 1,
+          volume: false
+        });
+
+        // Speaking shit
+        const speakingUsers = {};
+        connection.on("speaking", (user, speaking) => {
+          if (speaking.bitfield !== 0) {
+            console.log("ififififi");
+            console.log(speaking);
+
+            const audioStream = connection.receiver.createStream(user, {
+              mode: "pcm",
+              end: "silence"
+            });
+
+            let input = mixer.input({
+              channels: 2,
+              bitDepth: 16,
+              sampleRate: 48000,
+              volume: 100
+            });
+
+            audioStream.pipe(input);
+
+            speakingUsers[user.id] = input;
+          } else {
+            console.log("bingbingbongbong");
+            const input = speakingUsers[user.id];
+            if (input) {
+              mixer.removeInput(input);
+            }
+            speakingUsers[user.id] = undefined;
+          }
+        });
+        const ff2 = ffmpeg(mixer)
+          .inputFormat("s16le")
+          // .audioChannels(2)
+          .inputOptions("-ac 2")
+          .inputOptions("-ar 48.0k")
+          .inputOptions("-acodec pcm_s16le")
+          .outputFormat("pulse")
+          .outputOptions("-device mushit")
+          .outputOptions("-bufsize 512")
+          .output("suprise mothafucka!")
+          //.outputFormat("opus")
+          //.output("litershot.ogg")
+          .run();
+      } else {
+        message.reply("You need to join a voice channel first!");
+      }
+      break;
+  }
+});
+
+const botToken = process.env.DISCORD_BOT_TOKEN;
+client.login(botToken);
---
+++ docker-setup.md
@@ -0,0 +1,31 @@
+# Docker Setup for disspeak
+
+## Base Image
+
+`ubuntu:18.04`
+
+## System dependencies
+
+```bash
+apt update
+
+apt install xvfb
+apt install x11-xkb-utils
+apt install libglib2.0-0
+apt install libnss3
+apt install libfontconfig-dev
+apt install libxcomposite-dev
+apt install libxcursor1
+apt install libxi6
+apt install libxtst6
+apt install libxss1
+apt install libpci-dev
+apt install libasound2
+apt install libdbus-1-3
+apt install libxslt1-dev
+apt install libegl1
+
+apt install libxkbcommon-x11-0
+
+apt install ca-certificates
+```
